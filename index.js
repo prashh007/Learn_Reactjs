cd30c595b0645d9a7fdcd8746926189a82538e9b
prashh007 <prashanth2311145@gmail.com>
2019-07-30 11:13:19 +0530

    simple modified

--- index.js
+++ index.js
@@ -6,14 +6,17 @@ class App extends Component {
   constructor() {
     super();
     this.state = {
-      name: 'React for reactions.'
+      name: 'React for reaction',
+      task(){
+        return "learners.";
+      }
     };
   }
 
   render() {
     return (
       <div>
-       <h1>{this.state.name}</h1>
+       <h1>{this.state.name} {this.state.task()}</h1>
         <p>
           Start editing to see some magic happen :)
         </p>
